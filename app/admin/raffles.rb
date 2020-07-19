605dfe41e4e039def582268100ee8522b0f6012d
Alexei Mamani <alexeim763@gmail.com>
2020-07-19 11:31:02 -0500

    Added raffle model to activeadmin

---
+++ app/admin/raffles.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Raffle do
+  permit_params :name, :amount, :quantity
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    coulms :amount
+    column :quantity
+    actions
+  end
+
+  filter :name
+  filter :amount
+  filter :quantity
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :amount
+      f.input :quantity
+    end
+    f.actions
+  end
+end
